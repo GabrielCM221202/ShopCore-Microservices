72ade1450a97b295ba1e9329d3b449a4b3a2a65b
unknown <gabo22.zuky@gmail.com>
2026-06-02 13:14:17 -0600

    feat(catalog): crear entidad Product con esquema dinamico para MongoDB

---
+++ catalog-service/src/main/java/com/shopcore/catalog_service/model/Product.java
@@ -0,0 +1,43 @@
+package com.shopcore.catalog_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
+
+@Document(collection = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    private String id;
+
+    private String name;
+
+    @Indexed(unique = true)
+    private String slug;
+
+    @Field("category_id")
+    private String categoryId;
+
+    @Field("base_price")
+    private BigDecimal basePrice;
+
+    private List<String> images;
+
+    // Aquí ocurre la magia del esquema dinámico
+    private Map<String, Object> attributes;
+
+    @Field("is_active")
+    private boolean isActive;
+}
